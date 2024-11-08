c09f7f95d78fcd619c7c07b655cce623c405fda8
sobhan <sobhan168nourani@gmail.com>
2024-11-08 14:00:35 +0330

    add helper to project

---
+++ functions/helper.php
@@ -0,0 +1,22 @@
+<?php
+
+define("BASE_URL", "http://localhost/simple_blog_site/");
+
+function redirect($url){
+    header("location: " . BASE_URL . $url);
+    exit();
+}
+
+function asset($file){
+    return trim(BASE_URL, "/ ") . trim($file , "/ ");
+}
+
+function url($url){
+    return trim(BASE_URL, "/ ") . trim($url, "/ ");
+}
+
+function dd($obj){
+    var_dump($obj);
+}
+
+?>
