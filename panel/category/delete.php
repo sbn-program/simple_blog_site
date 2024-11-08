5c565dc52d521d4a2e2a21e57d9096b4ce632233
sobhan <sobhan168nourani@gmail.com>
2024-11-08 15:15:05 +0330

    update the delete page of category

--- panel/category/delete.php
+++ panel/category/delete.php
@@ -2,6 +2,15 @@
     require_once '../../functions/helper.php';
     require_once '../../functions/pdo_connection.php';
 
+    $query = "SELECT * FROM `categories` WHERE id = ?";
+    $statement = $pdo->prepare($query);
+    $statement->execute([$_GET['cat_id']]);
+    $category = $statement->fetch();
+
+    if ($category === false) {
+        redirect('panel/category');
+    }
+
     if (isset($_GET['cat_id']) && $_GET['cat_id'] !== "") {
         $query = "DELETE FROM `categories` WHERE id = ?";
         $statement = $pdo->prepare($query);
